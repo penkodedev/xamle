ad51bd6a0f597f7394b51c0cd6eabff988742765
rickramalho <email@penkode.com>
2025-07-28 19:24:36 +0200

    Cambios en FetchFromWP.ts

--- src/utils/FetchFromWP.ts
+++ src/utils/FetchFromWP.ts
@@ -2,7 +2,7 @@
 export async function fetchFromWP<T>(endpoint: string): Promise<T> {
     const base = process.env.NEXT_PUBLIC_WP_API;
     const res = await fetch(`${base}${endpoint}`);
-    if (!res.ok) throw new Error("Error al obtener datos de WordPress");
+    if (!res.ok) throw new Error("Error al cargar datos de WordPress");
     return res.json();
   }
   
